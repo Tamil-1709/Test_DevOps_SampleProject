75a0f68b6a18d8fc5d8b9331254896b5e0d4200c
Tamil-1709 <tamilselvan.mariappan@acsys.com>
2025-07-09 15:54:30 +0530

    Update plugin.py

--- my_sqlfluff_rules/plugin.py
+++ my_sqlfluff_rules/plugin.py
@@ -1,6 +1,7 @@
 from sqlfluff.core.plugin import hookimpl
- 
+
 @hookimpl
 def get_rules():
-    from my_sqlfluff_rules.rules import Rule_MySqlFluffRules_L114,Rule_MySqlFluffRules_L115,Rule_MySqlFluffRules_L118
-    return [Rule_MySqlFluffRules_L114,Rule_MySqlFluffRules_L115,Rule_MySqlFluffRules_L118]
+    print("✅ plugin.py loaded!")  # 👈 Add this debug print
+    from my_sqlfluff_rules.rules import Rule_MySqlFluffRules_L114, Rule_MySqlFluffRules_L115
+    return [Rule_MySqlFluffRules_L114, Rule_MySqlFluffRules_L115]
